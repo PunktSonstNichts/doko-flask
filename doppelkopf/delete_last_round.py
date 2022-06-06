9ae9fd32ea8c7b576c68fbbb207707a112f61131
Till Valentin Meyer-Arlt <t.meyer-arlt@campus.tu-berlin.de>
2022-06-06 17:28:26 +0200

    cleaned up the workspace just a lil bit

--- doppelkopf/delete_last_round.py
+++ doppelkopf/delete_last_round.py
@@ -4,6 +4,7 @@ from doppelkopf.database_constructors import Rounds, RoundsXPlayer
 
 def delete(game_id):
     rounds = Rounds.query.filter_by(game_id=game_id).all()
+    # round_id is primary key and therefore unique
     id = rounds[-1].round_id
 
     for rxp in RoundsXPlayer.query.filter_by(round_id=id):
